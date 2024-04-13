b7128f88aecf935a97b208bf75669a4837925d82
abhay734 <vermaabhay734@gmail.com>
2024-04-13 13:28:23 +0530

    Hello World confirguration

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -1,8 +1,5 @@
 package com.in28minutes.learnspringframework;
 
-import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.PacmanGame;
-
 public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
