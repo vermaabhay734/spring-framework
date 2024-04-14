e4f61e064bda38f78f4cedd6ad399a744a7c2a6e
abhay734 <vermaabhay734@gmail.com>
2024-04-14 23:08:04 +0530

    Created More Java Spring Beans

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -14,5 +14,8 @@ public class App02HelloWorldSpring {
 
 //		Retrieve beans managed by Spring
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
 	}
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfirguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfirguration.java
@@ -3,11 +3,29 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) { };
+record Address (String firstline, String city) { };
+
 @Configuration
 public class HelloWorldConfirguration {
 
     @Bean
     public  String name(){
         return "Abhay";
+    }
+
+    @Bean
+    public  int age(){
+        return 15;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("Ravi", 20);
+    }
+
+    @Bean
+    public Address address(){
+        return new Address("Kamla Nagar", "Agra");
     }
 }
