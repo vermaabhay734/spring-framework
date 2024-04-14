18eddd5de1be8826ccbdf1a8cc5e2a90a1464b46
abhay734 <vermaabhay734@gmail.com>
2024-04-14 22:54:54 +0530

    Print name with beans

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -1,9 +1,18 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 //		Launch a spring context
-//		Configure the thing that we want spring to manage. @Congfiguration
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfirguration.class);
+
+//		Configure the thing that we want spring to manage.
+//		HelloWorldConfiguration class - @Congfiguration
+//		name - @Bean
+
+//		Retrieve beans managed by Spring
+		System.out.println(context.getBean("name"));
 	}
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfirguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfirguration.java
@@ -1,7 +1,13 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfirguration {
+
+    @Bean
+    public  String name(){
+        return "Abhay";
+    }
 }
