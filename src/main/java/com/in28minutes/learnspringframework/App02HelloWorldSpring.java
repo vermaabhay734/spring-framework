640f9f6c376e4f2b8edaeaeba54ada47bffef0ac
abhay734 <vermaabhay734@gmail.com>
2024-04-15 15:38:22 +0530

    Implementing Auto Wiring in Spring

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -16,6 +16,9 @@ public class App02HelloWorldSpring {
 		System.out.println(context.getBean("name"));
 		System.out.println(context.getBean("age"));
 		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("address"));
+		System.out.println(context.getBean("person2MethodCall"));
+		System.out.println(context.getBean("person3Parameter"));
+		System.out.println(context.getBean("address2"));
+//		System.out.println(context.getBean(Address.class));
 	}
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfirguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfirguration.java
@@ -3,7 +3,7 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age) { };
+record Person (String name, int age, Address address) { };
 record Address (String firstline, String city) { };
 
 @Configuration
@@ -21,11 +21,26 @@ public class HelloWorldConfirguration {
 
     @Bean
     public Person person(){
-        return new Person("Ravi", 20);
+        return new Person("Ravi", 20, new Address("sector 52", "Noida"));
     }
 
     @Bean
+    public Person person2MethodCall(){
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameter(String name, int age, Address address3){
+        return new Person(name, age, address3);
+    }
+
+    @Bean (name = "address2")
     public Address address(){
         return new Address("Kamla Nagar", "Agra");
+    }
+
+    @Bean (name = "address3")
+    public Address address3(){
+        return new Address("Kalyanpur", "Kanpur");
     }
 }
