552d20701dde4863416818d0fa2c26c7ac39af8e
abhay734 <vermaabhay734@gmail.com>
2024-04-16 14:39:40 +0530

    Manage Beans for java gaming app

--- src/main/java/com/in28minutes/learnspringframework/HelloWorld/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorld/App02HelloWorldSpring.java
@@ -6,21 +6,21 @@ public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 //		Launch a spring context
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfirguration.class);
+		try(var context = new AnnotationConfigApplicationContext(HelloWorldConfirguration.class)){
+	//		Configure the thing that we want spring to manage.
+	//		HelloWorldConfiguration class - @Congfiguration
+	//		name - @Bean
 
-//		Configure the thing that we want spring to manage.
-//		HelloWorldConfiguration class - @Congfiguration
-//		name - @Bean
-
-//		Retrieve beans managed by Spring
-		System.out.println(context.getBean("name"));
-		System.out.println(context.getBean("age"));
-		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("person2MethodCall"));
-		System.out.println(context.getBean("person3Parameter"));
-		System.out.println(context.getBean("person4Parameter"));
-		System.out.println(context.getBean("person5Qualifier"));
-		System.out.println(context.getBean("address2"));
-		System.out.println(context.getBean(Address.class));
+	//		Retrieve beans managed by Spring
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("person2MethodCall"));
+			System.out.println(context.getBean("person3Parameter"));
+			System.out.println(context.getBean("person4Parameter"));
+			System.out.println(context.getBean("person5Qualifier"));
+			System.out.println(context.getBean("address2"));
+			System.out.println(context.getBean(Address.class));
+		}
 	}
 }
