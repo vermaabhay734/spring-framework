f087dde11eee6f5d3b07a4f1189736a1c9159cd6
abhay734 <vermaabhay734@gmail.com>
2024-04-16 13:04:08 +0530

    Moved HelloWorld to new package

--- src/main/java/com/in28minutes/learnspringframework/HelloWorld/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorld/App02HelloWorldSpring.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.HelloWorld;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
