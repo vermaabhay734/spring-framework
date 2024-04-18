6c50c24212a6d03aecaed44979657b97a5d8a1b4
abhay734 <vermaabhay734@gmail.com>
2024-04-18 15:47:36 +0530

    Merge configuration & App file together.

--- src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -4,6 +4,25 @@ import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class GamingConfiguration {
+
+	@Bean
+	public GamingConsole game(){
+		var game = new PacmanGame();
+		return game;
+	}
+
+	@Bean
+	public GameRunner gameRunner(GamingConsole game){
+		var gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
+}
+
 
 public class App03GamingSpringBeans {
 
