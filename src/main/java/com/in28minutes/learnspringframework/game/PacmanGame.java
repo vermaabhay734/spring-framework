b317928802b0d78bf89b274645103727ddb41d77
abhay734 <vermaabhay734@gmail.com>
2024-04-18 16:15:07 +0530

    Adding Components & ComponentScan

--- src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
+++ src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -1,5 +1,7 @@
 package com.in28minutes.learnspringframework.game;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PacmanGame implements GamingConsole {
     public void up(){
         System.out.println("Up");
